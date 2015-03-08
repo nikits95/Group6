e52906662f34ed0b1b68386ddfda05fda8382c22
Ryan Flynn <Ryan_Flynn@Ryans-MacBook-Pro.local>
2015-03-08 14:51:30 +0000

    more code

---
+++ CalResults.py
@@ -0,0 +1,32 @@
+class CalResults:
+	def __init__(self, data,typeQ):
+		self.data = data
+		self.typeQ = typeQ
+		print("The inforamton is : " + str(self.data))
+		if self.typeQ == "H&M":
+			self.honeyMumford(self.data)
+		else:
+			print("-----")
+
+	def honeyMumford(self, data):
+		#print("HoneyMumford")
+		occurance = {}
+		for i in [1,2,3,4]:
+			#print("Number : "+ str(i))
+			results = self.numberCounter(data,i)
+			
+			occurance.update({i:results})
+		print(occurance)
+
+		for key, item in occurance.items():
+			print(key, item)
+
+	def numberCounter(self,data,number):
+		numTimes = 0
+		for i in data:
+			#print("Compareing :"+str(i))
+			if i == number:
+				numTimes+= 1
+		return numTimes
+
+CalResults([2,4,2,1], "H&M")
