b7ce19e8e1c78350fe59623ea1d84bf4224cf910
Benajaxlewis <benajaxlewis@gmail.com>
2015-03-15 11:27:44 +0000

    Update home.py

--- home.py
+++ home.py
@@ -19,5 +19,19 @@ class home(Frame):
 		#produces a list of all the modules in COMSC
 		#Similar to the one on the sheet
 		#When clicked along with the start questionnaire button, it should save the degree program. 
+		lblProg = Label(self, text='Degree Programme:', font=('MS', 8,'bold'))
+		lblProg.grid(row=0, column=0, columnspan=2, sticky=N)
+
+		self.listProg = Listbox(self,height= 3)
+		scroll = Scrollbar(self, command= self.listProg.yview)
+		self.listProg.configure(yscrollcommand=scroll.set)
+
+		self.listProg.grid(row=0, column=2,columnspan=2,sticky=N)
+		scroll.grid(row=0, column=4, sticky=W)
+
+		for item in ["Computer Science", "Software Engineering","BIS",""]:
+			self.listProg.insert(END, item)
+		
+		self.listProg.selection_set(END)
 
 	#add any more methods if needed
