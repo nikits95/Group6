5f9a82ff1098b6ab092c74d501dbab765d9b15f5
Ryan Flynn <Ryan_Flynn@v1201-0a984e15.wifi.cf.ac.uk>
2015-03-16 13:49:32 +0000

    Update CalResults
    Now can detect what type a user is from the H&M Questionnaire.

--- CalResults.py
+++ CalResults.py
@@ -2,31 +2,45 @@ class CalResults:
 	def __init__(self, data,typeQ):
 		self.data = data
 		self.typeQ = typeQ
-		print("The inforamton is : " + str(self.data))
+		#print("The inforamton is : " + str(self.data))
 		if self.typeQ == "H&M":
 			self.honeyMumford(self.data)
 		else:
-			print("-----")
+			self.VARK(self.data)
+
+	def VARK(self,data):
+		print("")
 
 	def honeyMumford(self, data):
-		#print("HoneyMumford")
-		occurance = {}
+		occurance = []
 		for i in [1,2,3,4]:
-			#print("Number : "+ str(i))
 			results = self.numberCounter(data,i)
-			
-			occurance.update({i:results})
-		print(occurance)
+			occurance.append(results)
+		numMax = 1
+		compare = 0
+		for i in range(0,3):
+			if(occurance[i] > compare):
+				compare = occurance[i]
+				numMax = i
 
-		#for key, item in occurance.items():
-			#print(key, item)
+		typeOfLearningStyle = numMax+1
+		self.selectTypeHoneyStlye(typeOfLearningStyle)
+	
+	def selectTypeHoneyStlye(self, number):
+		if number == 1:
+			print("Reflector")
+		elif number == 2:
+			print("Pragmatist")
+		elif number == 3:
+			print("Theorist")
+		else:
+			print("Activist")
 
 	def numberCounter(self,data,number):
 		numTimes = 0
 		for i in data:
-			#print("Compareing :"+str(i))
 			if i == number:
 				numTimes+= 1
 		return numTimes
 
-#CalResults([2,4,2,1], "H&M") - For testing purposes
+#CalResults([2,4,4,2,1,2,2,1], "H&M")
