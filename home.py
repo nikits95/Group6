756c8f39ffa1b6ffb325ce527d9a48eecca5081b
Ryan Flynn <Ryan_Flynn@Ryans-MacBook-Pro.local>
2015-03-16 18:43:52 +0000

    Home Compelted

--- home.py
+++ home.py
@@ -1,37 +1,60 @@
 from tkinter import *
-class home(Frame):
-	def _init__(self,master):
-		Frame.__init_(self,master)
-		#call the methods for creating the Labels, buttons, etc here
-		#e.g. self.createHomeButtons()
 
-	def createHomeButtons(self):
-		#Creates Tk buttons
-		#One that leads to the first questionnaire
-		#The other that will move to the COMSC part
+class Home(Frame):
+  def __init__(self,master):
+    Frame.__init__(self,master)
+    alable = Label(self,text="Information")
+    alable.grid(row=1, column=1)
+    self.grid()
+    self.create_degree_program()
+    self.create_yearOfStudy()
+    self.create_button()
 
-	def createHomeLabels(self):
-		#creates the 3 main labels for the Frame
-		#One for instructions
-		#Another to instruct COMSC to click Admin buttom
+  def get_information(self):
+    #return str(self.varYear.get()), str(self.listProg.get(self.listProg.curselection()))
+    print(str(self.varYear.get()), str(self.listProg.get(self.listProg.curselection())))
+    Home.grid_forget(self)
 
-	def moduleProgram(self):
-		#produces a list of all the modules in COMSC
-		#Similar to the one on the sheet
-		#When clicked along with the start questionnaire button, it should save the degree program. 
-		lblProg = Label(self, text='Degree Programme:', font=('MS', 8,'bold'))
-		lblProg.grid(row=0, column=0, columnspan=2, sticky=N)
+  def create_degree_program(self):
+    lblDegree = Label(self, text="Degree Program")
+    lblDegree.grid(row=3, column=0, sticky=NE)
 
-		self.listProg = Listbox(self,height= 3)
-		scroll = Scrollbar(self, command= self.listProg.yview)
-		self.listProg.configure(yscrollcommand=scroll.set)
+    self.listProg = Listbox(self, height= 3)
+    scroll = Scrollbar(self, command= self.listProg.yview) 
+    self.listProg.configure(yscrollcommand=scroll.set)
+    self.listProg.grid(row=3, column=1, sticky=NE, rowspan=3) 
+    scroll.grid(row=3, column=4, sticky=W)
+    emptyLabel = Label(self) #used to make a break between listboxes
+    emptyLabel.grid(row=6,column=1)
 
-		self.listProg.grid(row=0, column=2,columnspan=2,sticky=N)
-		scroll.grid(row=0, column=4, sticky=W)
+    
+    for item in ["CS", "CS with", "BIS", "SE", "Joints"]: 
+      self.listProg.insert(END, item)
+    self.listProg.selection_set(END)
 
-		for item in ["Computer Science", "Software Engineering","BIS",""]:
-			self.listProg.insert(END, item)
-		
-		self.listProg.selection_set(END)
+  def create_yearOfStudy(self):
+    lblYear = Label(self, text="Current Year of Study")
+    lblYear.grid(row=8, column=0, sticky=NE)
 
-	#add any more methods if needed
+    self.varYear = StringVar()
+    R1 = Radiobutton(self, text="Year1", variable=self.varYear, value="Year1")
+    R1.grid(row=8, column= 1, sticky=W, padx=100)
+    R1.select()
+    R2 = Radiobutton(self,text="Year2", variable= self.varYear, value="Year2")
+    R2.grid(row=9, column= 1, sticky=W, padx=100)
+    R3 = Radiobutton(self, text="Year3", variable= self.varYear, value="Year3") 
+    R3.grid(row=10, column= 1, sticky=W, padx=100)
+    R4 = Radiobutton(self, text="Year4", variable= self.varYear, value="Year4") 
+    R4.grid(row=11, column= 1, sticky=W, padx=100)
+
+  def create_button(self):
+    btn = Button(self, text="Start Questionnaire", command=self.get_information)
+    btn.grid(row=12, column=1)
+
+def main():
+  window = Tk()
+  app = Home(window)
+  window.mainloop()
+
+if __name__ == '__main__':
+  main()
