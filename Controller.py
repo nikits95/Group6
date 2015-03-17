2f1a74ecbc64171774f48e3b0f4c6ac350f64c1e
Ryan Flynn <Ryan_Flynn@Ryans-MacBook-Pro.local>
2015-03-17 18:19:25 +0000

    Updated Controller

--- Controller.py
+++ Controller.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from Home import *
-from Questionnaire import *
 
 class Controller(Frame):
 	
@@ -8,20 +7,23 @@ class Controller(Frame):
 		Frame.__init__(self, master)
 		self.root = master
 
-		cont_frame = Frame(self.root)
-		cont_frame.pack(side=LEFT, padx=5, pady=5)
+		self.cont_frame = Frame(self.root)
+		self.cont_frame.pack(side=LEFT, padx=5, pady=5)
 
-		homepage = Home(cont_frame)
-		homepage.pack(side=TOP, padx=20, pady=20)
-		
-		btn = Button(cont_frame, text="Submit", command= lambda: self.homeData(homepage))
-		btn.pack()
+		homepage = Home(self.cont_frame)
+		homepage.pack(side=LEFT, padx=20, pady=20)
 
-	def homeData(self, f):
-		(a,b) = f.get_information()
-		print(a,b)
-		f.destroy()
-		self.nextQues()
+		self.btnHome = Button(self.cont_frame, text="Submit", command= lambda: self.gettingHomeData(homepage))
+		self.btnHome.pack(side=LEFT)
+
+	def gettingHomeData(self, currentframe):
+		(yearOfStudy,degreeProgram) = currentframe.get_information()
+		print(yearOfStudy,degreeProgram)
+		currentframe.destroy()
+		self.btnHome.destroy()
+
+		#honey = Questionnaire(self.cont_frame) #the first questionnaire needs 
+		#honey.pack()							#to go here
 
 
 
